15fb69aabfaf5ba45de7bcff586c43be123c4460
AutoGuResRaPeJu <rpererah@gmail.com>
2020-06-13 00:01:20 -0300

    new

--- components/SignOut/luzes/index.js
+++ components/SignOut/luzes/index.js
@@ -1,38 +1,68 @@
 import React,{useState} from 'react'
 import { View,TouchableOpacity,Switch, Text, Dimensions } from 'react-native'
 import MenuBar from './../menubar'
-import Ionicons from 'react-native-vector-icons/Ionicons'
 import Switchb from './switch'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-function luzes({navigation}) {
+function Mdispositivos({navigation}){
     const altura=Dimensions.get('screen').height
- 
     const AbraTeSesamo=()=>{
         navigation.openDrawer();
     }
-    return (
-        <>
-        <View>
+    return(
+<View>
 <MenuBar>
     <TouchableOpacity onPress={AbraTeSesamo}>
       <Ionicons name={'md-menu'} size={40} color={'white'}/>
     </TouchableOpacity>
 </MenuBar>
+        
         </View>
-        <View style={{backgroundColor:'#C6C9D2',height:altura,alignContent:'center',}}>
-            <View>
-            <Text style={{fontSize:28,textAlign:'center',justifyContent:'center',alignItems:'center',alignContent:'center'}}>Suas Lampadas</Text>
-            </View>
-       <View style={{alignItems:'flex-start',flexDirection:'row'}}>
+    )
+}
 
-       <Switchb/>
-       </View>
-       <View style={{flexDirection:'row'}}>
+function Portas({navigation}){
+return(
+    <View>
+        <Text>OI</Text>
+    </View>
+);
+}
 
-<Switchb/>
-</View>
-        </View>
+function luzes({navigation}) {
+    
+    const Tab = createBottomTabNavigator();
+    
+    return (
+        <>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Campainha" component={Mdispositivos} />
+        <Tab.Screen name="Luzes" component={Portas} />
+        <Tab.Screen name="Despertados" component={Portas} />
+      </Tab.Navigator>
         </>
     )
 }
