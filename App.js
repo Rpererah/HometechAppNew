9e1dde491f2551ad77bab464e85a1791462a5076
AutoGuResRaPeJu <rpererah@gmail.com>
2020-06-14 01:24:40 -0300

    FirebaseNotification OK

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
 import React from 'react'
 import Routes from './Routes/Routes'
 import AutenticaProvider from './contexts/Autenticacao';
 
 function App() {
+  const defaultAppMessaging = messaging();
   return (
     <NavigationContainer>
           <AutenticaProvider>
